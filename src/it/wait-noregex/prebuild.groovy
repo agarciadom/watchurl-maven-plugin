a39b8dd6c26801d1137ff1688cd0f5a913ee9c47
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2013-09-15 00:48:16 +0200

    Add integration tests

---
+++ src/it/wait-noregex/prebuild.groovy
@@ -0,0 +1,19 @@
+import fi.iki.elonen.NanoHTTPD
+import fi.iki.elonen.NanoHTTPD.Response
+import fi.iki.elonen.NanoHTTPD.Method
+import java.util.Map
+
+NanoHTTPD server = new NanoHTTPD(8080) {
+  @Override
+  public Response serve(String uri, Method method, Map<String, String> headers, Map<String, String> params, Map<String, String> files) {
+    return new Response("Hello world!")
+  }
+}
+context.put("server", server)
+
+new Thread(new Runnable() {
+  public void run() {
+    Thread.sleep(2000)
+    server.start()
+  }
+}).start()
--- src/it/wait-regex/prebuild.groovy
+++ src/it/wait-regex/prebuild.groovy
@@ -4,10 +4,18 @@ import fi.iki.elonen.NanoHTTPD.Method
 import java.util.Map
 
 NanoHTTPD server = new NanoHTTPD(8080) {
+  boolean first = true
+
   @Override
   public Response serve(String uri, Method method, Map<String, String> headers, Map<String, String> params, Map<String, String> files) {
-    return new Response("Hello world!");
+    return new Response(true ? "Hello world!" : "Bye world!")
   }
-};
+}
+context.put("server", server)
 
-server.start()
+new Thread(new Runnable() {
+  public void run() {
+    Thread.sleep(2000)
+    server.start()
+  }
+}).start()
