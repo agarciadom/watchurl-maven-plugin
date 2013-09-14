c2d9ff26e5195349a40ece52736329638476e931
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2013-09-14 23:24:00 +0200

    Add NanoHTTPd to test classpath and do some prep work

---
+++ src/it/simple-it/prebuild.groovy
@@ -0,0 +1,13 @@
+import fi.iki.elonen.NanoHTTPD
+import fi.iki.elonen.NanoHTTPD.Response
+import fi.iki.elonen.NanoHTTPD.Method
+import java.util.Map
+
+NanoHTTPD server = new NanoHTTPD(8080) {
+  @Override
+  public Response serve(String uri, Method method, Map<String, String> headers, Map<String, String> params, Map<String, String> files) {
+    return new Response("Hello world!");
+  }
+};
+
+server.start()
